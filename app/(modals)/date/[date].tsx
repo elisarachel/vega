6c5c98f2930db103f47de1896d3578939409e486
Elisa Rachel <elisarachel2@gmail.com>
2025-04-22 16:33:37 -0300

    Ajustes no espaçamento

--- app/(modals)/date/[date].tsx
+++ app/(modals)/date/[date].tsx
@@ -249,7 +249,7 @@ const styles = StyleSheet.create({
 	},
 	eventsContainer: {
 		padding: Math.round(6*scaleFactor),
-		gap: Math.round(6*scaleFactor)
+		gap: Math.round(4*scaleFactor)
 	},
 	eventText: {
 		fontFamily: 'TinyUnicode',
@@ -270,7 +270,7 @@ const styles = StyleSheet.create({
 		fontFamily: 'Tiny5',
 		fontSize: Math.round(7 * scaleFactor),
 		color: '#18122B',
-		marginBottom: Math.round(4 * scaleFactor),
+		//marginBottom: Math.round(4 * scaleFactor),
 		textTransform: 'uppercase'
 	},
 	moonPhaseCanvas: {
@@ -281,7 +281,7 @@ const styles = StyleSheet.create({
 		fontFamily: 'TinyUnicode',
 		fontSize: Math.round(12 * scaleFactor),
 		color: '#7A7D8D',
-		marginTop: Math.round(4 * scaleFactor),
+		//marginTop: Math.round(4 * scaleFactor),
 		textTransform: 'capitalize',
 	},
 	sectionTitle: {
@@ -307,7 +307,7 @@ const styles = StyleSheet.create({
 	weatherIcon: {
 		width: Math.round(16 * scaleFactor),
 		height: Math.round(16 * scaleFactor),
-		marginBottom: Math.round(4 * scaleFactor),
+		//marginBottom: Math.round(4 * scaleFactor),
 	},
 	weatherDescription: {
 		fontFamily: 'TinyUnicode',
